bb7caa4e0e9ce83e0a8d140c05a2838e657c7718
strategic101 <strategicskillset@gmail.com>
2025-01-27 21:59:17 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,173 @@
+let dsb =new Swiper("#swiper-2", {
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 60,
+  pagination: {
+      el: ("#swiper-2 .swiper-custom-pagination"),
+      clickable: true,
+      renderBullet: function (index, className) {
+      return `<div class=${className}>
+          <span class="number">${index + 1}</span>
+          <span class="line"></span>
+          </div>`;
+      }
+  },
+  autoplay: {
+    delay: 7000,
+  },
+  lazyLoading: true,
+  loop: true,
+  keyboard: {
+      enabled: true,
+  },
+  navigation: {
+      nextEl: "#nav-right",
+      prevEl: "#nav-left"
+  },
+  breakpoints: {
+      800: {
+          slidesPerView: 1.5,
+      },
+      
+    spaceBetween: 60,
+  }
+});
+
+
+// Second slider
+
+new Swiper("#swiper-3", {
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+      el: ("#swiper-2 .swiper-custom-pagination"),
+      clickable: true,
+      renderBullet: function (index, className) {
+      return `<div class=${className}>
+          <span class="number">${index + 1}</span>
+          <span class="line"></span>
+          </div>`;
+      }
+  },
+  autoplay: {
+    delay: 7000,
+  },
+  lazyLoading: true,
+  loop: true,
+  keyboard: {
+      enabled: true,
+  },
+  navigation: {
+      nextEl: "#nav-right",
+      prevEl: "#nav-left"
+  },
+  breakpoints: {
+      800: {
+          slidesPerView: 3.5,
+      },
+      
+    spaceBetween: 60,
+  }
+});
+
+new Swiper("#swiper-4", {
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+      el: ("#swiper-2 .swiper-custom-pagination"),
+      clickable: true,
+      renderBullet: function (index, className) {
+      return `<div class=${className}>
+          <span class="number">${index + 1}</span>
+          <span class="line"></span>
+          </div>`;
+      }
+  },
+  autoplay: {
+    delay: 7000,
+  },
+  lazyLoading: true,
+  loop: true,
+  keyboard: {
+      enabled: true,
+  },
+  navigation: {
+      nextEl: "#nav-right",
+      prevEl: "#nav-left"
+  },
+  breakpoints: {
+      800: {
+          slidesPerView: 5,
+      },
+      
+    spaceBetween: 60,
+  }
+});
+
+
+
+const pauseBtn=document.querySelector('.pause');
+const playBtn=document.querySelector('.play');
+pauseBtn.addEventListener('click', function(){
+  dsb.autoplay.stop();
+  playBtn.style.display = 'inline-block';
+  pauseBtn.style.display = 'none';
+})
+
+playBtn.addEventListener('click', function(){
+  dsb.autoplay.start();
+  pauseBtn.style.display = 'inline-block';
+  playBtn.style.display = 'none';
+})
+
+
+//NAVBAR SECTION
+
+let l1=document.querySelector('.li1');
+let l2=document.querySelector('.li2');
+let l3=document.querySelector('.li3');
+
+let closebtn=document.querySelectorAll('.closesect');
+
+let firstsection=document.querySelector('.whatwedocontent');
+let secondsection=document.querySelector('.whatweare');
+let thirdsection=document.querySelector('.insights');
+
+l1.addEventListener('mouseenter',function(){
+  firstsection.classList.add('active');
+})
+
+l2.addEventListener('mouseenter',function(){
+  secondsection.classList.add('active');
+})
+
+
+l3.addEventListener('mouseenter',function(){
+  thirdsection.classList.add('active');
+})
+
+console.log(closebtn);
+closebtn.forEach(function(item){
+ item.addEventListener('click', function(){
+    console.log("working");
+    firstsection.classList.remove('active');
+    secondsection.classList.remove('active');
+    thirdsection.classList.remove('active');
+  })
+})
+
+
+//Navbar
+let menuopenclose = document.querySelector('.menubarbtn');
+let navbar=document.querySelector('.navbar');
+// let closenavbtn=document.querySelector('.closenavbtn');
+
+menuopenclose.addEventListener('click',function(){
+  navbar.classList.toggle('active');
+  menuopenclose.classList.toggle('active');
+
+})
+
 
