489698677b446685600825f856bc0dbeab05c8c1
strategic101 <strategicskillset@gmail.com>
2025-02-07 10:34:17 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -171,3 +171,25 @@ menuopenclose.addEventListener('click',function(){
 })
 
 
+const swiper3 = new Swiper(".slider-3", {
+  autoHeight: true,
+  effect: "coverflow",
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 40,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  navigation: {
+    nextEl: ".custom-next",
+    prevEl: ".custom-prev",
+  },
+  pagination: {
+    el: ".custom-pagination",
+  },
+});
